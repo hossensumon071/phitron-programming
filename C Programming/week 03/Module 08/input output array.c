e77770343753ba78ba7a21641a6bfe3007801722
hossensumon071 <55638643+hossensumon071@users.noreply.github.com>
2022-11-15 14:10:19 +0600

    learning c programming

---
+++ C Programming/week 03/Module 08/input output array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int ara[5];
+  int i;
+  for(i=0; i<5; i++)
+  {
+    scanf("%d", &ara[i]);
+  }
+
+  for(i=0; i<5; i++)
+  {
+    printf("%d-th position value = %d\n", i, ara[i]);
+  }
+  return 0;
+}
